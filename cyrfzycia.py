24c21abffe058a5abfe88034ab6db42b021e8b62
Przemek B <pprzemek89@gmail.com>
2024-03-07 10:47:59 +0100

    Anagramy

--- cyrfzycia.py
+++ cyrfzycia.py
@@ -1,11 +1,4 @@
-dzien = int(input("Podaj dzien"))
-miesiac = input("podaj miesiac: ")
-rok = int(input("podaj rok:"))
-klucz = {"styczen":1,"luty":2,"marzec":3,"kwiecien":4,"maj":5,"czerwiec":6,"lipiec":7,"sierpien":8,"wrzesien":9,"pazdziernik":10,"listopad":11,"grudzien":12}
-miesiacjakoliczba = 0
-for klucz,wartosc in klucz.items():
-    if miesiac == klucz:
-        miesiacjakoliczba = wartosc
+dataurodzin = int(input("podaj w formacie rr/mm/dd "))
 
-wynik = dzien+miesiacjakoliczba+rok
-print(wynik)
+
+
