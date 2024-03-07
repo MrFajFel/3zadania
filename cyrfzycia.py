093d4a0c0bfa474e5f54bfb8e02a954d916c84cc
Przemek B <pprzemek89@gmail.com>
2024-03-07 10:58:03 +0100

    Anagramy

--- cyrfzycia.py
+++ cyrfzycia.py
@@ -1,4 +1,15 @@
-dataurodzin = int(input("podaj w formacie rr/mm/dd "))
+dataurodzin = input("podaj w formacie rr/mm/dd ")
+liczba = 0
+liczba2 = 0
+for i in range(len(dataurodzin)):
+    liczba = liczba + int(dataurodzin[i])
+print(liczba)
+
+wynik = str(liczba)
+for i in range(len(wynik)):
+    liczba2 = liczba2 + int(wynik[i])
+print(liczba2)
+
 
 
 
